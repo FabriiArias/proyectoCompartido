9cf6e92e0414369b54bbab8c242a918895030167
Fabrizio Arias <fabrisal@live.com.ar>
2024-03-19 17:17:05 -0300

    Saludo
    agregue un sout que dice algo mucho muy importante

--- src/proyectocompartido/ProyectoCompartido.java
+++ src/proyectocompartido/ProyectoCompartido.java
@@ -7,7 +7,8 @@ public class ProyectoCompartido {
          
         for (int i = 0; i < numeros.length; i++) {
             System.out.println(numeros[i]);
-        }                 
+        }
+        System.out.println("Hola");
     }
     
 }
