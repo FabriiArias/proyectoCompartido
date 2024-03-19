04ed7eca5c0320389a85670c2a7654277efa7d57
Fabrizio Arias <fabrisal@live.com.ar>
2024-03-19 17:20:58 -0300

    saludo
    como va?

--- src/proyectocompartido/ProyectoCompartido.java
+++ src/proyectocompartido/ProyectoCompartido.java
@@ -8,7 +8,7 @@ public class ProyectoCompartido {
         for (int i = 0; i < numeros.length; i++) {
             System.out.println(numeros[i]);
         }
-        System.out.println("Hola");
+        System.out.println("Hola, como va?");
     }
     
 }
